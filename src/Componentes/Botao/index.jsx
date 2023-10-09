04661500b3c31a556fa046a17874b2338f9bb962
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-10-09 09:44:56 -0300

    .

--- src/Componentes/Botao/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import style from './botao.module.css'
-
-export default function Botao() {
-  return (
-    <a href="https://kiwify.app/JGzjGmT?afid=ipqehkJ6" target="_blank" className={style.botao}>IR PARA O SITE OFICIAL</a>
-  )
-}
