f1d8adfdfc9fc77f4d10400e8efe10767685a004
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-10-12 17:45:40 -0300

    .

--- src/Componentes/Botao/index.jsx
+++ src/Componentes/Botao/index.jsx
@@ -3,6 +3,6 @@ import style from './botao.module.css'
 
 export default function Botao() {
   return (
-    <a href="https://kiwify.app/JGzjGmT?afid=ipqehkJ6" target="_blank" className={style.botao}>IR PARA O SITE OFICIAL</a>
+    <a href="https://kiwify.app/JGzjGmT?afid=ipqehkJ6" className={style.botao}>IR PARA O SITE OFICIAL</a>
   )
 }
